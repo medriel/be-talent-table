5b3532354380e24e0a4d95e92c983bf918f761b0
medriel <adriel.medeiros@madeiramadeira.com.br>
2025-07-12 15:49:10 -0300

    feat: create SearchContainer component

---
+++ src/components/SearchContainer/index.tsx
@@ -0,0 +1,13 @@
+import SearchInput from "../SearchInput";
+import "./styles.css";
+
+const SearchContainer = () => {
+  return (
+    <div className="table-header">
+      <h1>Funcionários</h1>
+      <SearchInput />
+    </div>
+  );
+};
+
+export default SearchContainer;
